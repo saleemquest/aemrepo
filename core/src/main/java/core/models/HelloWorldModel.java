e4b08a1f2bdb5c55c3e32cd9848a50a23474addd
Mahammad Saleem Tapal <Saleem.Tapal@neudesic.com>
2017-03-09 15:01:48 +0530

    added message

--- core/src/main/java/core/models/HelloWorldModel.java
+++ core/src/main/java/core/models/HelloWorldModel.java
@@ -13,6 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+ //If you have questions, please open an issue
 package ${package}.core.models;
 
 import javax.annotation.PostConstruct;
