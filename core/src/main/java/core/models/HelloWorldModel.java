e69536d396cbd3c7c939129737841297bde623c9
Mahammad Saleem Tapal <Saleem.Tapal@neudesic.com>
2017-03-09 15:10:02 +0530

    add message from two

--- core/src/main/java/core/models/HelloWorldModel.java
+++ core/src/main/java/core/models/HelloWorldModel.java
@@ -13,6 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+ // If you have questions ask your question in IRC
 package ${package}.core.models;
 
 import javax.annotation.PostConstruct;
